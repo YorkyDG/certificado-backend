9f117a100a52b08e6288168e2a102deab3b179b8
MisaelGuzman <misaelguzman159@gmail.com>
2024-06-03 21:23:28 -0400

    Se crearon los end-points para el auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import * as authCtrl from "../controllers/login.controller.js";
 
 const router = Router();
 
-
+router.post("/signup", authCtrl.signUp);
+router.post("/signin", authCtrl.signIn);
 
 
 
