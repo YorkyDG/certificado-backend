8529a3bf6b39b0c35afee1d1e726dc846f8bfdf9
MisaelGuzman <misaelguzman159@gmail.com>
2024-06-03 21:18:35 -0400

    se importaron las rutas del auth

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,16 +11,12 @@ app.use(morgan("dev", "start"));
 app.use(express.json());
 app.use(cors());
 
+app.use("/api", authRoutes);
 app.get("/", (req, res) => {
   console.log(`Running on PORT ${PORT}`);
   res.json("UwU");
 });
 
-app.post("/register", (req, res) => {
-  console.log(req.body);
-  const { name, cedula, email, tel, password, department } = req.body;
-  res.json("UwU");
-});
 
 app.listen(PORT, (req, res) => {
   console.log(`Listening on port ${PORT}`);
